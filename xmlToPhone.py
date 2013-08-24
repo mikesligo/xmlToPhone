935e8945837af5cc9e99d5644b1ce3a8e3841187
Mike Gallagher <mikesligo@gmail.com>
2013-08-24 16:52:22 +0100

    copying directory structure, really need to add classes and functions

--- xmlToPhone.py
+++ xmlToPhone.py
@@ -1,18 +1,36 @@
 import sys
 import re
 from subprocess import call
+import os
+from os import path
+import shutil
 
 xml_file = sys.argv[1]
 print "XML File is " + xml_file
 
+music_dir = '/home/mike/Music'
 url_locs = []
 with open (xml_file, 'r') as read_file:
     for line in read_file:
-        search = re.search('<location>file://(.*)</location>', line)
+        search = re.search('<location>file://'+music_dir+'(.*)</location>', line)
         if search is not None:
             url_locs.append(search.group(1))
 
-number_to_copy = len(url_locs)
-for idx, file_loc in enumerate(url_locs):
-    print "(" + str(idx+1) + "/" + str(number_to_copy) + ")" + "\t\tPushing " + file_loc + "..."
-    call(["adb","push", file_loc,"/sdcard/Music"])
+temporary_music_dir = path.abspath('.tmpmusic')
+if not os.path.exists(temporary_music_dir):
+    os.makedirs(temporary_music_dir)
+
+print "Creating temporary directory structure..."
+for file_loc in url_locs:
+    actual_path = path.abspath(music_dir + file_loc).replace('&amp;','&')
+    new_song_dir_loc = path.abspath(temporary_music_dir + file_loc[:file_loc.rfind('/')]).replace('&amp;','&')
+    new_song_loc = path.abspath(temporary_music_dir+file_loc).replace('&amp;','&')
+    if not path.exists(new_song_dir_loc):
+        os.makedirs(new_song_dir_loc)
+    if not path.exists(new_song_loc):
+        shutil.copy(actual_path, new_song_loc)
+"...done"
+
+print "Pushing all files..."
+call(["adb","push", temporary_music_dir,"/sdcard/Music"])
+print "...done"
